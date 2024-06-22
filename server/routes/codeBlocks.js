b98b5a1fefea02888c18cbe7dbaf2847cdaf417d
GalKoaz <koazgal@gmail.com>
2024-06-23 02:31:18 +0300

    remove unnecessary code ( clean code )

--- server/routes/codeBlocks.js
+++ server/routes/codeBlocks.js
@@ -42,38 +42,4 @@ router.get("/block/:id", async (req, res) => {
   }
 });
 
-// router.post("/", async (req, res) => {
-//     const codeBlock = new CodeBlock({
-//         title: req.body.title,
-//         code: req.body.code,
-//         solution: req.body.solution,
-//         blockid: req.body.blockid
-//     });
-
-//     try {
-//         const savedCodeBlock = await codeBlock.save();
-//         res.json(savedCodeBlock);
-//     } catch (error) {
-//         res.json({ message: error });
-//     }
-// });
-
-// router.get("/:codeBlockId", async (req, res) => {
-//     try {
-//         const codeBlock = await CodeBlock.findById(req.params.codeBlockId);
-//         res.json(codeBlock);
-//     } catch (error) {
-//         res.json({ message: error });
-//     }
-// });
-
-// router.delete("/:codeBlockId", async (req, res) => {
-//     try {
-//         const removedCodeBlock = await CodeBlock.remove({ _id: req.params.codeBlockId });
-//         res.json(removedCodeBlock);
-//     } catch (error) {
-//         res.json({ message: error });
-//     }
-// });
-
 module.exports = router;
